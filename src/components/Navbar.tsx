090eefd7e57caf7bb4cb326fc76b73af46c389f0
akilinjamam <akilinjamam@gmail.com>
2024-10-01 02:59:20 +0600

    file-updated-with-navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,17 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { homeRoutes, THomeRoutes } from "./homeRoutes";
+import { useState } from "react";
 
 const Navbar = () => {
-   
+
+   const [hide, setHide] = useState(true);
 
     const pathName = usePathname();
    console.log(pathName)
     return (
-        <div className="w-full h-[50px] bg-gray-100 flex items-center justify-between">
-            <div className="w-[24.5%] h-full bg-gray-200">
+        <div className="w-full h-[50px] bg-gray-100 flex items-center justify-between ">
+            <div className="w-[24.5%] h-full ">
                
             </div>
            <div className="w-[50%] h-full flex items-center justify-around">
@@ -30,8 +32,13 @@ const Navbar = () => {
                     })
                 }
            </div>
-            <div className="w-[24.5%] h-full bg-gray-200">
+            <div className="w-[24.5%] h-full flex items-center justify-end px-2 relative">
+                <div onClick={() => setHide(!hide)} className="w-[40px] h-[40px] bg-gray-200 rounded-full cursor-pointer">
 
+                </div>
+                <div className={`w-[200px] h-[100px] bg-gray-100 top-[60px] right-0 ${hide ? 'hidden' : 'absolute'}`}>
+
+                </div>
             </div>
         </div>
     );
