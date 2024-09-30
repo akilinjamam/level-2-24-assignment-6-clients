2be59c8125623053d94095ed792b31ff566f9566
akilinjamam <akilinjamam@gmail.com>
2024-10-01 01:50:40 +0600

    file-updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,36 @@
-"use client"
-import clsx from 'clsx'; // Import clsx or classnames library
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { homeRoutes, THomeRoutes } from "./homeRoutes";
 
 const Navbar = () => {
+   
+
     const pathName = usePathname();
-
-    const active = (linkValue: string) => {
-        return pathName === linkValue;
-    };
-
+   console.log(pathName)
     return (
         <div className="w-full h-[50px] bg-gray-100 flex items-center justify-between">
-            <div className="w-[24.5%] h-full bg-gray-200"></div>
-            <div className="w-[50%] h-full flex items-center justify-around">
-                {homeRoutes?.map((item: THomeRoutes, index: number) => {
-                    return (
-                        <div
-                            key={index + 1}
-                            className={clsx("w-[50%] h-full flex items-center justify-around", {
-                                "border-b-2 border-blue-500": active(item.links),
-                            })}
-                        >
-                            <Link href={item.links}>
-                                <i
-                                    title={item?.name}
-                                    className={`${item?.icons} text-2xl w-[24%] text-center cursor-pointer`}
-                                ></i>
-                            </Link>
-                        </div>
-                    );
-                })}
+            <div className="w-[24.5%] h-full bg-gray-200">
+               
             </div>
-            <div className="w-[24.5%] h-full bg-gray-200"></div>
+           <div className="w-[50%] h-full flex items-center justify-around">
+                {
+                    homeRoutes?.map((item: THomeRoutes, index: number) => {
+                       
+                        return (
+                            <div style={{borderBottom: `${(pathName === item?.links) ? '2px solid blue' : ''}`}}  key={index + 1} className=" w-[50%] h-full flex items-center justify-around">
+                                <Link  href={item.links}>
+                                    <i title={item?.name} className={`${item?.icons} text-2xl w-[24%] text-center cursor-pointer`}></i>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+           </div>
+            <div className="w-[24.5%] h-full bg-gray-200">
+
+            </div>
         </div>
     );
 };
