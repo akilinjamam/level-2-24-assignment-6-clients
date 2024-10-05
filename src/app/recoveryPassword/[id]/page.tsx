16b18812312dc3704e4a89b505bdb3ba83174dad
akilinjamam <akilinjamam@gmail.com>
2024-10-06 02:50:19 +0600

    file-updated

--- src/app/recoveryPassword/[id]/page.tsx
+++ src/app/recoveryPassword/[id]/page.tsx
@@ -105,12 +105,12 @@ const RecoveryPassword = () => {
                 <div className="lg:w-[50%] md:w-[50%] sm:w-full xsm:w-full h-[100%] bg-gray-100">
 
                 <section className="px-5 py-5">
-                    <p  className="text-gray-700 text-3xl font-bold my-6"> {9 > 0 ? `Recover Password: will be expired after : ${9} minutes` : `Recovery Password Expired`}</p>
+                    <p  className="text-gray-700 text-3xl font-bold my-6"> {9 > 0 ? `Recover Password: will be expired after : ${remainingMinutes} minutes` : `Recovery Password Expired`}</p>
                     <hr />
                     <br />
                     <form onSubmit={handleSubmit(onSubmit)}>
                         
-                        <input value={`opticalsoft21@gmail.com`} style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="email" {...register("email")} placeholder='type your email' />
+                        <input value={email} style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="email" {...register("email")} placeholder='type your email' />
                         {errors.email && <span>This field is required</span>}
                         <br />
                         <input style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="password"  {...register("password")} placeholder='type password' />
