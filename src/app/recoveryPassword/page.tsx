c6cc1ac37af9345cdea4221e3d32c7a7859ac421
akilinjamam <akilinjamam@gmail.com>
2024-10-05 18:18:21 +0600

    file-updated

--- src/app/recoveryPassword/page.tsx
+++ src/app/recoveryPassword/page.tsx
@@ -6,7 +6,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { useUserRecoveryPassword, } from "@/hooks/auth.hook";
 import { useRouter, useSearchParams } from "next/navigation";
 import { jwtDecode, JwtPayload } from "jwt-decode";
-import { Suspense, useEffect } from "react";
+import {  useEffect } from "react";
 
 interface CustomJwtPayload extends JwtPayload {
     email?: string;
@@ -94,38 +94,36 @@ const RecoveryPassword = () => {
 
 
     return (
-        <Suspense  fallback={<div>Loading...</div>}>
-            <div className="w-[100%] h-auto flex justify-center">
-                <div className="lg:w-[50%] md:w-[70%] sm:w-[100%] xsm:w-[100%] h-[500px] bg-gray-100 my-5 lg:flex md:flex">
-                    <div className="lg:w-[50%] md:w-[50%] sm:w-full xsm:w-full h-[100%] bg-gray-200 p-5">
+        <div className="w-[100%] h-auto flex justify-center">
+            <div className="lg:w-[50%] md:w-[70%] sm:w-[100%] xsm:w-[100%] h-[500px] bg-gray-100 my-5 lg:flex md:flex">
+                <div className="lg:w-[50%] md:w-[50%] sm:w-full xsm:w-full h-[100%] bg-gray-200 p-5">
 
-                        <section className="w-full h-[100%] flex items-center justify-center">
-                            {View}
-                        </section>
-
-                    </div>
-                    <div className="lg:w-[50%] md:w-[50%] sm:w-full xsm:w-full h-[100%] bg-gray-100">
-
-                    <section className="px-5 py-5">
-                        <p  className="text-gray-700 text-3xl font-bold my-6">Recover Password: will be expired : {remainingMinutes}</p>
-                        <hr />
-                        <br />
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            
-                            <input value={email} style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="email" {...register("email")} placeholder='type your email' />
-                            {errors.email && <span>This field is required</span>}
-                            <br />
-                            <input style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="password"  {...register("password")} placeholder='type password' />
-                            <br />
-                            <br />
-                            <input className='w-[100px] h-[35px] rounded-md bg-blue-500 text-white font-bold cursor-pointer' type="submit" value={`${isPending ? 'loading' : 'SUBMIT'}`} />
-                        </form>
+                    <section className="w-full h-[100%] flex items-center justify-center">
+                        {View}
                     </section>
 
-                    </div>
+                </div>
+                <div className="lg:w-[50%] md:w-[50%] sm:w-full xsm:w-full h-[100%] bg-gray-100">
+
+                <section className="px-5 py-5">
+                    <p  className="text-gray-700 text-3xl font-bold my-6">Recover Password: will be expired : {remainingMinutes}</p>
+                    <hr />
+                    <br />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        
+                        <input value={email} style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="email" {...register("email")} placeholder='type your email' />
+                        {errors.email && <span>This field is required</span>}
+                        <br />
+                        <input style={{background:'none',borderBottom:'1px solid lightgray'}} className='mb-3 w-[80%] ' type="password"  {...register("password")} placeholder='type password' />
+                        <br />
+                        <br />
+                        <input className='w-[100px] h-[35px] rounded-md bg-blue-500 text-white font-bold cursor-pointer' type="submit" value={`${isPending ? 'loading' : 'SUBMIT'}`} />
+                    </form>
+                </section>
+
                 </div>
             </div>
-        </Suspense>
+        </div>
     );
 };
 
