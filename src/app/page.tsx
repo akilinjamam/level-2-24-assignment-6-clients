b53afa28ba8c5f5779c7c2e789544f2766b6e503
akilinjamam <akilinjamam@gmail.com>
2024-10-14 17:34:46 +0600

    file-updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use server";  
 
+import { TPosts } from "@/types/posts.type";
 import { cookies } from "next/headers";
-
-import NewsFeeds from "@/components/newsFeeds/NewsFeeds";
+import Image from "next/image";
+import fallbackImg from '../../images/default-fallback-image.png';
+import LightGalleryImage from "@/components/LightGallery";
+import Follow from "@/components/follow/Follow";
 
 const Home = async () => {
 
@@ -36,7 +39,53 @@ const Home = async () => {
     const data = await getPosts();
 
     return (
-        <NewsFeeds data={data} accessToken={accessToken}/>
+        <div className="w-full min-h-[1200px] my-2">
+            <div className="w-[50%] min-h-[1200px] bg-gray-100 mx-auto p-2">
+                {data?.data?.map((item: TPosts, index: number) => (
+                    <div className="bg-gray-200 mb-3" key={index + 1}>
+                        <div className="w-full h-[50px] flex items-center justify-between px-2">
+                            <div className="w-[200px] flex items-center font-bold">
+                                <div className="w-[35px] h-[35px] rounded-full bg-gray-300 overflow-hidden">
+                                    <Image className="scale-125 mt-2" width={200} height={200} src={item?.userId?.profileImg || fallbackImg} alt="" />
+                                </div>
+                                <div className="ml-3">
+                                    <p>{item?.name}</p>
+                                </div>
+                            </div>
+                            <Follow myId={accessToken} followingId={item?.userId?._id}/>
+                        </div>
+
+                        {/* Image section */}
+                        <div className="w-full h-auto">
+                            <LightGalleryImage item={item} />
+                        </div>
+                        <br />
+
+                        {/* Title */}
+                        <div className="w-full ml-1">
+                            <p className="font-bold">{item?.title}</p>
+                        </div>
+                        <br />
+
+                        {/* Description */}
+                        <div className="w-full ml-1">
+                            <div dangerouslySetInnerHTML={{ __html:item?.description  }} />
+                            
+                        </div>
+                        <br />
+
+                        <div className="w-full h-[30px] px-1 flex items-center justify-between font-bold text-blue-500">
+                            <div className="bg-gray-300 w-[49.5%] h-[30px] flex items-center justify-center">
+                                <p>Up Vote</p>
+                            </div>
+                            <div className="bg-gray-300 w-[50%] h-[30px] flex items-center justify-center">
+                                <p>Down Vote</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
