0e06153e8db70821d4d572e7fa6a574ff0f66956
akilinjamam <akilinjamam@gmail.com>
2024-10-18 03:18:39 +0600

    file-updated

--- src/components/ProfileImg.tsx
+++ src/components/ProfileImg.tsx
@@ -2,13 +2,14 @@
 import '../app/globals.css';
 import { useGetProfileUser, useUpdateCover, useUpdateProfile } from '@/hooks/profile.hook';
 import Image from 'next/image';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import fallbackImg from '../../images/default-fallback-image.png';
 // import { useAppContext } from '@/contextApi';
 import { useGetFollow } from '@/hooks/follow.hook';
 import { CustomJwtPayload } from '@/jwtDecoder/jwtDecoder';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import AddPostModal from './modal/AddPostModal';
 
 type followingIds = {
     _id:string;
@@ -33,6 +34,7 @@ export type TFollow = {
 
 
 const ProfileImg = ({userInfo} : {userInfo: CustomJwtPayload}) => {
+    const [open, setOpen] = useState(false)
     const router = useRouter();
     const {data} = useGetFollow()
     // const [favorite, setFavourite] = useState(false);
@@ -79,69 +81,79 @@ const ProfileImg = ({userInfo} : {userInfo: CustomJwtPayload}) => {
 
 
     return (
-        <div className="w-[50%] bg-gray-200 mx-auto auto my-2 h-auto">
-            <section className="w-full h-[400px] bg-gray-200 relative overflow: 'hidden'">
-                <div className='w-full h-[400px] overflow-hidden '>
-                    <Image style={{width:'100%', height:'auto'}} width={1000} height={700} priority src={userData?.coverImg === 'add cover img' ? fallbackImg : userData?.coverImg || fallbackImg}  alt='cover-image'/>
-                </div>
-                <div className="w-[300px] h-[300px] rounded-full bg-gray-300 absolute left-[20px] bottom-[-70px] viewWithHover z-10 overflow-hidden ">
-                    <Image className='absolute inset-x-10 inset-y-12 w-full h-full object-cover scale-150 ' style={{width:'100%', height:'auto'}} width={300} height={300} priority src={userData?.profileImg === ('add profile img') ? (fallbackImg) : userData?.profileImg || fallbackImg}  alt='profile-image'/>
-                    <label  htmlFor="image">
-                        <i className="uil uil-pen absolute bottom-[70px] right-[30px] hide cursor-pointer bg-gray-200 px-2 rounded-full text-blue-500"></i>
+        <div>
+            <div className="w-[50%] bg-gray-200 mx-auto auto my-2 h-auto">
+                <section className="w-full h-[400px] bg-gray-200 relative overflow: 'hidden'">
+                    <div className='w-full h-[400px] overflow-hidden '>
+                        <Image style={{width:'100%', height:'auto'}} width={1000} height={700} priority src={userData?.coverImg === 'add cover img' ? fallbackImg : userData?.coverImg || fallbackImg}  alt='cover-image'/>
+                    </div>
+                    <div className="w-[300px] h-[300px] rounded-full bg-gray-300 absolute left-[20px] bottom-[-70px] viewWithHover z-10 overflow-hidden ">
+                        <Image className='absolute inset-x-10 inset-y-12 w-full h-full object-cover scale-150 ' style={{width:'100%', height:'auto'}} width={300} height={300} priority src={userData?.profileImg === ('add profile img') ? (fallbackImg) : userData?.profileImg || fallbackImg}  alt='profile-image'/>
+                        <label  htmlFor="image">
+                            <i className="uil uil-pen absolute bottom-[70px] right-[30px] hide cursor-pointer bg-gray-200 px-2 rounded-full text-blue-500"></i>
+                        </label>
+                        <input onChange={handleProfileImageChange} className='hidden' type="file" name="" id="image" />
+                    </div>
+                    <label htmlFor="coverImg">
+                        <i className="uil uil-pen absolute bottom-[20px] right-[20px] cursor-pointer text-blue-500 bg-gray-200 px-2 hover:bg-gray-300 rounded-full"></i>
                     </label>
-                    <input onChange={handleProfileImageChange} className='hidden' type="file" name="" id="image" />
-                </div>
-                <label htmlFor="coverImg">
-                    <i className="uil uil-pen absolute bottom-[20px] right-[20px] cursor-pointer text-blue-500 bg-gray-200 px-2 hover:bg-gray-300 rounded-full"></i>
-                </label>
-                <input onChange={handleCoverImageChange} className='hidden' type="file" name="" id="coverImg" />
-            </section>
-            <section className='w-full mx-auto bg-green h-auto'>
-                <div className='w-[95%] h-auto mt-[100px] mx-auto'>
-                    <p className='text-2xl font-bold border-b-2 border-gray-500 py-3'>{userData?.name}</p>
-                    <br />
+                    <input onChange={handleCoverImageChange} className='hidden' type="file" name="" id="coverImg" />
+                </section>
+                <section className='w-full mx-auto bg-green h-auto'>
+                    <div className='w-[95%] h-auto mt-[100px] mx-auto'>
+                        <p className='text-2xl font-bold border-b-2 border-gray-500 py-3'>{userData?.name}</p>
+                        <br />
+                        
+                        <p className='text-1xl font-bold  text-blue-400'>Following: {findFollowingData?.length}</p>
+                        <br />
+                        <div className='flex flex-wrap'>
+                            {
+                                findFollowingData?.map((item:TFollow, index:number) => {
+                                    return (
+                                        <div onClick={() =>handleNavigate(item?.id?._id)} key={index+1} className='w-[100px] h-[140px] bg-blue-400 mr-2 rounded overflow-hidden relative p-1 cursor-pointer'>
+                                            <Image className='w-[100%] h-[100%] object-cover scale-150' width={200} height={200} src={item?.id?.profileImg || fallbackImg } alt='profile-img'/>
+                                            <p title={item?.id?.name} className='absolute bottom-4 left-1 text-white text-sm bg-blue-500 rounded p-1 '>{item?.id?.name?.length > 11 ? item?.id?.name?.slice(0,9) + '..' : item?.id?.name }</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <br />
+                        <br />
+                        <p className='text-1xl font-bold  text-blue-400'>Followers: {findFollowerData?.length}</p>
+                        <br />
+                        <div className='flex flex-wrap'>
+                            {
+                                findFollowerData?.map((item:TFollow, index:number) => {
+                                    return (
+                                        <div  onClick={() =>handleNavigate(item?.follow?._id)} key={index+1} className='w-[100px] h-[140px] bg-blue-400 mr-2 rounded overflow-hidden relative p-1 cursor-pointer'>
+                                            <Image className='w-[100%] h-[100%] object-cover scale-150' width={200} height={200} src={item?.follow?.profileImg || fallbackImg } alt='profile-img'/>
+                                            <p title={item?.follow?.name} className='absolute bottom-4 left-1 bg-blue-500 p-1 rounded  text-white text-sm'>{item?.follow?.name?.length > 11 ? item?.follow?.name?.slice(0,9) + '..' : item?.follow?.name }</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <br />
                     
-                    <p className='text-1xl font-bold  text-blue-400'>Following: {findFollowingData?.length}</p>
-                    <br />
-                    <div className='flex flex-wrap'>
-                        {
-                            findFollowingData?.map((item:TFollow, index:number) => {
-                                return (
-                                    <div onClick={() =>handleNavigate(item?.id?._id)} key={index+1} className='w-[100px] h-[140px] bg-blue-400 mr-2 rounded overflow-hidden relative p-1 cursor-pointer'>
-                                        <Image className='w-[100%] h-[100%] object-cover scale-150' width={200} height={200} src={item?.id?.profileImg || fallbackImg } alt='profile-img'/>
-                                        <p title={item?.id?.name} className='absolute bottom-4 left-1 text-white text-sm bg-blue-500 rounded p-1 '>{item?.id?.name?.length > 11 ? item?.id?.name?.slice(0,9) + '..' : item?.id?.name }</p>
-                                    </div>
-                                )
-                            })
-                        }
+                        <div className='flex items-center justify-between w-full h-[50px]'>
+                            <div className='w-[300px] h-[50px]'>
+                                <Link href={`/profile`}>
+                                    <button className='text-white font-bold px-2 py-1 bg-blue-500 rounded mr-2'>Your Posts</button>
+                                </Link>
+                                <Link href={`/profileForFavPosts`}>
+                                    <button className='text-white font-bold px-2 py-1 bg-blue-500 rounded'>Favourite Posts</button>
+                                </Link>
+                            </div>
+                            <div>
+                                <p onClick={() => setOpen(true)} className='w-[130px] px-2 py-1 rounded bg-blue-500 font-bold text-white cursor-pointer'><i className="uil uil-plus"></i> Create Post</p>
+                            </div>
+                        </div>
+                        <br /><br />
                     </div>
-                    <br />
-                    <br />
-                    <p className='text-1xl font-bold  text-blue-400'>Followers: {findFollowerData?.length}</p>
-                    <br />
-                    <div className='flex flex-wrap'>
-                        {
-                            findFollowerData?.map((item:TFollow, index:number) => {
-                                return (
-                                    <div  onClick={() =>handleNavigate(item?.follow?._id)} key={index+1} className='w-[100px] h-[140px] bg-blue-400 mr-2 rounded overflow-hidden relative p-1 cursor-pointer'>
-                                        <Image className='w-[100%] h-[100%] object-cover scale-150' width={200} height={200} src={item?.follow?.profileImg || fallbackImg } alt='profile-img'/>
-                                        <p title={item?.follow?.name} className='absolute bottom-4 left-1 bg-blue-500 p-1 rounded  text-white text-sm'>{item?.follow?.name?.length > 11 ? item?.follow?.name?.slice(0,9) + '..' : item?.follow?.name }</p>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    <br />
-                    {/* <p onClick={() => setOpen(true)} className='w-[130px] px-2 py-1 rounded-full bg-gray-400 font-bold cursor-pointer'><i className="uil uil-plus"></i> Create Post</p> */}
-                    <Link href={`/profile`}>
-                        <button className='text-white font-bold px-2 py-1 bg-blue-500 rounded mr-2'>Your Posts</button>
-                    </Link>
-                    <Link href={`/profileForFavPosts`}>
-                        <button className='text-white font-bold px-2 py-1 bg-blue-500 rounded'>Favourite Posts</button>
-                    </Link>
-                    <br /><br />
-                </div>
-            </section>
+                </section>
+            </div>
+            <AddPostModal userInfo={userInfo} open={open} setOpen={setOpen}/>
         </div>
     );
 };
--- src/components/modal/AddPostModal.tsx
+++ src/components/modal/AddPostModal.tsx
@@ -1,21 +1,22 @@
-// "use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
 
-// import { CustomJwtPayload } from "@/jwtDecoder/jwtDecoder";
-// import AddPost from "../post/AddPost";
-// import { useAppContext } from "@/contextApi";
+import { CustomJwtPayload } from "@/jwtDecoder/jwtDecoder";
+import AddPost from "../post/AddPost";
 
-// const AddPostModal = ({userInfo} : {userInfo:CustomJwtPayload}) => {
 
-//     const {setOpen, open} = useAppContext()
+const AddPostModal = ({userInfo, open, setOpen} : {userInfo:CustomJwtPayload, open:boolean, setOpen:any}) => {
 
-//     return (
-//         <div className={` w-full h-full z-20 flex items-center justify-center bg-black bg-opacity-85 ${open ? 'fixed top-0' : 'hidden' }`}>
-//             <i onClick={() => setOpen(false)} className="uil uil-times absolute top-5 right-5 text-4xl text-gray-200 cursor-pointer"></i>
-//             <div className="w-[50%] h-[500px] overflow-x-hidden overflow-y-scroll bg-gray-200">
-//                  <AddPost userInfo={userInfo}/>
-//             </div>
-//         </div>
-//     );
-// };
+   
 
-// export default AddPostModal;
+    return (
+        <div className={` w-full h-full z-20 flex items-center justify-center bg-black bg-opacity-85 ${open ? 'fixed top-0' : 'hidden' }`}>
+            <i onClick={() => setOpen(false)} className="uil uil-times absolute top-5 right-5 text-4xl text-gray-200 cursor-pointer"></i>
+            <div className="w-[50%] h-[500px] overflow-x-hidden overflow-y-scroll bg-gray-200">
+                 <AddPost userInfo={userInfo}/>
+            </div>
+        </div>
+    );
+};
+
+export default AddPostModal;
